ed160784af67524e57d455401f699b978a431224
Rod Sheeter <rsheeter@google.com>
2019-03-05 11:42:24 -0800

    Drop gvar content

--- Snippets/partial.py
+++ Snippets/partial.py
@@ -8,17 +8,16 @@ from fontTools import ttLib
 import sys
 
 def _subset_gvar(gvar, axes_to_drop):
+  variations = {}
   for glyph_name, tuple_variations in gvar.variations.items():
-    for tuple_variation in tuple_variations:
-      drop = bool(tuple_variation.axes.keys() & axes_to_drop)
-      action = 'DROP'
-      if not drop:
-        action = 'KEEP'
-      print('{} {} for {}'.format(
-            glyph_name, action, sorted(tuple_variation.axes.keys())))
+    new_tuple_variations = [tv for tv in tuple_variations 
+                            if not tv.axes.keys() & axes_to_drop]
+    if new_tuple_variations:
+      variations[glyph_name] = new_tuple_variations
+  gvar.variations = variations
 
 axes_to_drop = {a for a in sys.argv[1:] if len(a) == 4}
-print('Dropping %s'.format(axes_to_drop))
+print('Dropping {}'.format(axes_to_drop))
 
 for arg in sys.argv[1:]:
   if len(arg) == 4:
@@ -26,5 +25,8 @@ for arg in sys.argv[1:]:
   with contextlib.closing(ttLib.TTFont(arg)) as font:
     print(arg)
     if 'gvar' in font:
-      font['gvar'] = _subset_gvar(font['gvar'], axes_to_drop)
+      _subset_gvar(font['gvar'], axes_to_drop)
+      subset_file = arg + '.subset'
+      font.save(subset_file)
+      print('Wrote {}'.format(subset_file))
 
